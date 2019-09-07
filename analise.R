4e807505b19537f1aa021b85b13668410c70a3ec
matheus <mateusmferreira@live.com>
2019-09-06 23:04:08 -0300

    Teste de geração de gráficos

--- analise.R
+++ analise.R
@@ -2,25 +2,23 @@ library(ggplot2)
 library(dplyr)
 
 # Efetua a leitura dos dados coletados do site INMET com as informações meteorologiocas da cidade de iguape
-iguape_met <- read.csv(file="C:/projeto/datasets/Iguape-2018-2019.csv", header=TRUE, sep=";")
+iguape_met <- read_csv(file="C:/projeto/datasets/Iguape-2018-2019.csv", header=TRUE, sep=";")
+summary(iguape_met)
 
 # Efetua a leitura dos dados coletados do KAGGLE sobre temperatura na região Sudeste
 suldoeste_met <- read.csv("C:/projeto/datasets/datasetSaoPaulo.csv", header=TRUE, sep=",")
+summary(suldoeste_met)
 
 # Efetua a leitura dos dados de solo
 solo <- read.csv("C:/projeto/datasets/Dataset-Solo.csv", header=TRUE, sep=";")
+summary(solo)
 
 # Efetua a limpeza dos campos que contem '////'
-iguape_met_clean <- iguape_met %>% filter(temp_inst != "////")
+iguape_met_clean <- iguape_met %>% filter(temp_inst != '////' | precipitacao != '////')
 
-# Efetua limpeza dos registros pelos ano de 2016
+# Efetua limpeza dos registros acima de 2015
 suldoeste_met_clean <- suldoeste_met %>% filter(yr > 2015)
 
+plot(iguape_met_clean$precipitacao)
 
-strDates <- as.character(iguape_met_clean$data)
-
-x <- strsplit(strDates, "/")
-
-View(x)
-teste <- iguape_met_clean %>% separate()
-
+aggregate(temp_inst ~ codigo_estacao, iguape_met_clean, sum)
